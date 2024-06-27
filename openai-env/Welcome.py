7c184bda470829c81e2c66bd384a5810826d6b29
DrReg <nikolav706@gmail.com>
2024-06-27 19:12:03 +0200

    Dodat sadrzaj na Welcome stranici koji opisuje ciljeve i namjenu platforme

--- openai-env/Welcome.py
+++ openai-env/Welcome.py
@@ -1,14 +1,48 @@
 import streamlit as st
+from PIL import Image
 
 
 
 def main():
     # Glavni setup
-    st.title("AI Learning Assistant Platform")
-    st.sidebar.success("Select a page above.")
+    st.set_page_config(page_title="AI Researcher - Online Platform", page_icon=":🧑‍🔬:")
 
-    # Dvije kolone dugmadi
-    col1, col2 = st.columns(2)
+    st.title("Kompjuter uči uz tebe!")
+    st.sidebar.success("Izaberite bilo koju stranicu iznad.")
+
+    st.write("Dobro došli na platformu AI Istraživača!")
+    st.write(""" Na ovoj stranici možeš da koristiš **vještačku inteligenciju** da naučiš nove teme i
+             usavršiš postojeće znanje sve uz pomoć *naših asistenata* - specijalizovanih vještačkih inteligencija osmišljeni kao pomoć studentima,
+             profesorima i svim radoznalim osobama.""")
+    
+    image = Image.open("studybuddy.png")
+    st.image(image, caption="Vaš lični digitalni profesor.", use_column_width=True)
+
+    st.header("News Summarizer")
+
+    st.write("Ovaj asistent ima ulogu pružanja sažetaka na temu koju korisnik izabere.")
+    st.write("Trenutno podržava funckije:")
+    st.write("""
+             - *Sažete pretrage članaka novosti*
+             - *Filtriranje na osnovu datuma, ključne riječi i kategorije*
+             - *Vraćanje URL adresa ako korisnik želi da pročita izvorni članak*
+             
+             """)
+    
+    st.header("Study Buddy")
+
+    st.write("Pametni asistent koji na osnovu datoteke koje korisnik zakači ima mogućnost konverzacije.")
+    st.write("Trenutno podržava funckije:")
+    st.write("""
+             - *Odgovora na pitanja*
+             - *Pisanje sažetaka*
+             - *Izdvajanje citata*
+             - *Osmišljavanje pitanja*
+             
+             """)
+    
+    st.warning("Možete doprinijeti projektu na našem GitHub nalogu: https://github.com/DrReg/News-Summarizer")
+
 
  
 
